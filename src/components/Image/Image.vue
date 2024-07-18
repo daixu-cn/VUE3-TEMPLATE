14f1b10c50ccb57c8afb69928bf4f9f47f3ccd9c
daixu <daixu.cn@outlook.com>
2024-07-18 09:36:39 +0800

    feat: Add Image and ImageViewer components

---
+++ src/components/Image/Image.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-image
+    v-bind="$props"
+    @load="$emit('load', $event)"
+    @error="$emit('error', $event)"
+    @switch="$emit('switch', $event)"
+    @close="$emit('close')"
+    @show="$emit('show')"
+  >
+    <template #placeholder>
+      <el-skeleton animated>
+        <template #template>
+          <div class="placeholder-template">
+            <el-skeleton-item variant="rect" />
+            <el-icon class="is-loading"><Loading /></el-icon>
+          </div>
+        </template>
+      </el-skeleton>
+    </template>
+    <template #error>
+      <el-skeleton>
+        <template #template>
+          <div class="error-template">
+            <el-skeleton-item variant="rect" />
+            <el-icon><Picture /></el-icon>
+          </div>
+        </template>
+      </el-skeleton>
+    </template>
+    <template #[name] v-for="(slot, name) in $slots" :key="name">
+      <component :is="slot" />
+    </template>
+  </el-image>
+</template>
+
+<script setup lang="ts">
+import { ElImage, ElSkeleton, ElSkeletonItem, ElIcon, imageEmits } from "element-plus"
+import type { ImageProps } from "element-plus"
+import { Picture, Loading } from "@element-plus/icons-vue"
+
+defineSlots<{ placeholder(): void; error(): void; viewer(): void }>()
+defineEmits(imageEmits)
+withDefaults(defineProps<Partial<ImageProps>>(), {
+  fit: "cover",
+  loading: "lazy",
+  previewTeleported: true,
+})
+</script>
+
+<style lang="scss">
+.el-image {
+  width: 100%;
+  height: 100%;
+  color: $font-secondary-color;
+
+  .el-skeleton {
+    width: 100%;
+    height: 100%;
+
+    .placeholder-template,
+    .error-template {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      @include flex-center;
+
+      .el-skeleton__item {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Image/ImageViewer.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-image-viewer
+    v-if="$props.urlList?.length"
+    ref="ImageViewerRef"
+    v-bind="$props"
+    @close="$emit('close')"
+    @switch="$emit('switch', $event)"
+    @rotate="$emit('rotate', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+import { ElImageViewer, imageViewerEmits } from "element-plus"
+import type { ImageViewerProps, ImageViewerInstance } from "element-plus"
+
+const ImageViewerRef = ref<ImageViewerInstance>()
+defineEmits(imageViewerEmits)
+withDefaults(defineProps<Partial<ImageViewerProps>>(), { teleported: true })
+defineExpose({ instance: ImageViewerRef })
+</script>
