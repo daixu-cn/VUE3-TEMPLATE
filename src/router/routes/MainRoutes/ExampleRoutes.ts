fcc78703ee570e22dc03d362a24e5fe7b9cf54e2
daixu <daixu.cn@outlook.com>
2024-08-02 14:34:26 +0800

    feat: Example 路由增加认证校验

--- src/router/routes/MainRoutes/ExampleRoutes.ts
+++ src/router/routes/MainRoutes/ExampleRoutes.ts
@@ -2,22 +2,30 @@ import type { RouteRecordRaw } from "vue-router"
 
 const ExampleRoutes: Readonly<RouteRecordRaw[]> = [
   {
-    path: "/example/chart",
-    name: "Chart",
-    component: () => import("@/views/Main/Example/Chart.vue"),
-    meta: { title: "图表" },
-  },
-  {
-    path: "/example/form",
-    name: "EditableForm",
-    component: () => import("@/views/Main/Example/EditableForm.vue"),
-    meta: { title: "可编辑表单" },
-  },
-  {
-    path: "/example/upload",
-    name: "Upload",
-    component: () => import("@/views/Main/Example/Upload.vue"),
-    meta: { title: "文件上传" },
+    path: "/example",
+    name: "Example",
+    meta: { auth: true },
+    redirect: "/example/chart",
+    children: [
+      {
+        path: "/example/chart",
+        name: "Chart",
+        component: () => import("@/views/Main/Example/Chart.vue"),
+        meta: { title: "图表" },
+      },
+      {
+        path: "/example/form",
+        name: "EditableForm",
+        component: () => import("@/views/Main/Example/EditableForm.vue"),
+        meta: { title: "可编辑表单" },
+      },
+      {
+        path: "/example/upload",
+        name: "Upload",
+        component: () => import("@/views/Main/Example/Upload.vue"),
+        meta: { title: "文件上传" },
+      },
+    ],
   },
 ]
 
