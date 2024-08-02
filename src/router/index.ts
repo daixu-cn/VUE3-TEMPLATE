6404bb3e731f1c5f1641aaa7ea577cfa558ee68a
daixu <daixu.cn@outlook.com>
2024-08-02 15:56:18 +0800

    feat: 设置页面默认滚动行为

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ import { hasPermission } from "@/tools/permission"
 const router = createRouter({
   history: createWebHistory(BASE_URL),
   routes,
+  scrollBehavior(to, from, saved) {
+    if (saved) return saved
+    if (to.hash) return { el: to.hash, behavior: "smooth" }
+    return { top: 0, left: 0, behavior: "smooth" }
+  },
 })
 
 router.beforeEach((to, from, next) => {
